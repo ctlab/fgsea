ba722fced3dc0014012b066ce2c11954f9c2af30
Alexey Sergushichev <alsergbox@gmail.com>
2016-06-20 19:01:35 +0300

    missed documentation

---
+++ R/fgsea-package.R
@@ -0,0 +1,13 @@
+#' @useDynLib fgsea
+#' @import Rcpp
+NULL
+
+#' Example vector of gene-lvele statistics obtained for Th1 polarization
+#' @docType data
+#' @name exampleRanks
+NULL
+
+#' Example list of reactome mouse pathways
+#' @docType data
+#' @name examplePathways
+NULL
