7a2fcf3e18d5a617c8db6e7beb91b46e2b3d7020
Alexey Sergushichev <alsergbox@gmail.com>
2016-11-20 21:32:59 +0000

    v1.1.2
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/fgsea@124300 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,3 +1,8 @@
+# setting R_TESTS to empty string because of
+# https://github.com/hadley/testthat/issues/144
+# revert this when that issue in R is fixed.
+Sys.setenv("R_TESTS" = "")
+
 library(testthat)
 library(fgsea)
 
