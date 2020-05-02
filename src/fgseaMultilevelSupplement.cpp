67eeb035cfe921a509461fd6c3f814193750041c
Nikolay Budin <n.budin@yandex.ru>
2020-05-02 16:45:44 +0300

    1 year old perturbate

--- src/fgseaMultilevelSupplement.cpp
+++ src/fgseaMultilevelSupplement.cpp
@@ -190,111 +190,186 @@ pair<double, bool> EsRuler::getPvalue(double ES, double eps, bool sign) {
 
 
 
-int perturbate(const vector<double> &ranks, vector<int> &sample, vector<double> &chunkSum, vector<int> &chunkSize,
-               double bound, mt19937 &rng) {
-    double pertPrmtr = 0.1;
-    int n = (int) ranks.size();
-    int k = (int) sample.size();
-    uniform_int_distribution<> uid_n(0, n - 1);
-    uniform_int_distribution<> uid_k(0, k - 1);
-    double NS = 0;
-    for (int pos : sample) {
-        NS += ranks[pos];
+double cross(int x1, double y1, int x2, double y2) {
+  return x1 * y2 - y1 * x2;
+}
+
+struct genes {
+  vector<double> const& ranks;
+  int chunk_length;
+  vector<int> inds;
+  vector<int> next;
+  vector<int> first_in_chunk, size_of_chunk;
+  vector<double> sum_in_chunk;
+  int diagonal_x;
+  double diagonal_y;
+
+  genes(vector<double> const& _ranks, vector<int> const& _inds) : ranks(_ranks), chunk_length((int) (ranks.size() / sqrt(_inds.size()))), 
+    inds(_inds), next(inds.size(), -1), first_in_chunk((ranks.size() + chunk_length - 1) / chunk_length, -1), size_of_chunk(first_in_chunk.size()), sum_in_chunk(first_in_chunk.size()),
+    diagonal_x(ranks.size() - inds.size()), diagonal_y(0) {
+    int chunk = -1;
+    for (int i = 0; i < inds.size(); ++i) {
+      diagonal_y += ranks[inds[i]];
+      int cur = inds[i] / chunk_length;
+      size_of_chunk[cur]++;
+      sum_in_chunk[cur] += ranks[inds[i]];
+      if (cur > chunk) {
+        chunk = cur;
+        first_in_chunk[chunk] = i;
+      } else {
+        next[i - 1] = i;
+      }
     }
-    double q1 = 1.0 / (n - k);
-    int iters = max(1, (int) (k * pertPrmtr));
-    int moves = 0;
-    for (int i = 0; i < iters; i++) {
-        int id = uid_k(rng);
-        int oldInd = sample[id];
-        int newInd = uid_n(rng);
-        NS = NS - ranks[oldInd] + ranks[newInd];
+  }
 
-        sample[id] = newInd;
-        while (id > 0 && sample[id] < sample[id - 1]) {
-            swap(sample[id], sample[id - 1]);
-            id--;
+  // void remove_pos(int, int);
+  // void add_ind(int, int, int);
+  // int add_ind(int, int);
+  bool check_es_not_less(double bound) {
+    bound *= diagonal_x * diagonal_y;
+    double y = 0;
+    int x = 0;
+    int prev = 0;
+    for (int i = 0; i < first_in_chunk.size(); ++i) {
+      if (cross(diagonal_x, diagonal_y, x, y + sum_in_chunk[i]) <= bound) {
+        x += chunk_length * (i + 1) - prev - size_of_chunk[i];
+        y += sum_in_chunk[i];
+        prev = (i + 1) * chunk_length;
+        continue;
+      }
+      int pos = first_in_chunk[i];
+      while (pos != -1) {
+        x += inds[pos] - prev;
+        y += ranks[inds[pos]];
+        prev = inds[pos] + 1;
+        pos = next[pos];
+        if (cross(diagonal_x, diagonal_y, x, y) > bound) {
+          return true;
         }
-        while (id < k - 1 && sample[id] > sample[id + 1]) {
-            swap(sample[id], sample[id + 1]);
-            id++;
-        }
-
-        if ((id > 0 && sample[id] == sample[id - 1]) || (id < k - 1 && sample[id] == sample[id + 1])) {
-            // revert changes...
-            sample[id] = oldInd;
-            while (id > 0 && sample[id] < sample[id - 1]) {
-                swap(sample[id], sample[id - 1]);
-                id--;
-            }
-            while (id < k - 1 && sample[id] > sample[id + 1]) {
-                swap(sample[id], sample[id + 1]);
-                id++;
-            }
-            NS = NS - ranks[newInd] + ranks[oldInd];
-            continue;
-        }
-
-        int oldChunkInd = upper_bound(chunkLastElement.begin(), chunkLastElement.end(), oldInd) - chunkLastElement.begin();
-        int newChunkInd = upper_bound(chunkLastElement.begin(), chunkLastElement.end(), newInd) - chunkLastElement.begin();
-
-        chunkSum[oldChunkInd] -= ranks[oldInd];
-        chunkSize[oldChunkInd]--;
-
-        chunkSum[newChunkInd] += ranks[newInd];
-        chunkSize[newChunkInd]++;
-
-        double cur = 0.0;
-        double q2 = 1.0 / NS;
-        int last = -1;
-        int cnt = 0;
-        int lastChunk = 0;
-
-        bool ok = false;
-
-        for (int i = 0; i < chunksNumber; ++i) {
-            if (cur + q2 * chunkSum[i] < bound) {
-                cur = cur + q2 * chunkSum[i] - q1 * (chunkLastElement[i] - lastChunk - chunkSize[i]);
-                last = chunkLastElement[i] - 1;
-                cnt += chunkSize[i];
-            } else {
-                while (cnt < sample.size() && sample[cnt] < chunkLastElement[i]) {
-                    int pos = sample[cnt];
-                    cur += q2 * ranks[pos] - q1 * (pos - last - 1);
-                    if (cur > bound) {
-                        ok = true;
-                        break;
-                    }
-                    last = pos;
-                    ++cnt;
-                }
-                if (ok) {
-                    break;
-                }
-            }
-            lastChunk = chunkLastElement[i];
-        }
-        
-        if (!ok) {
-            sample[id] = oldInd;
-            while (id > 0 && sample[id] < sample[id - 1]) {
-                swap(sample[id], sample[id - 1]);
-                id--;
-            }
-            while (id < k - 1 && sample[id] > sample[id + 1]) {
-                swap(sample[id], sample[id + 1]);
-                id++;
-            }
-            NS = NS - ranks[newInd] + ranks[oldInd];
-            
-            chunkSum[oldChunkInd] += ranks[oldInd];
-            chunkSize[oldChunkInd]++;
-
-            chunkSum[newChunkInd] -= ranks[newInd];
-            chunkSize[newChunkInd]--;
-        } else {
-            ++moves;
-        }
+      }
     }
-    return moves;
+
+    return false;
+  }
+
+  vector<int> get_inds() {
+    vector<int> ret;
+    for (int i = 0; i < first_in_chunk.size(); ++i) {
+      int pos = first_in_chunk[i];
+      while (pos != -1) {
+        ret.push_back(inds[pos]);
+        pos = next[pos];
+      }
+    }
+    return ret;
+  }
+
+  bool try_replace(int order_number, int new_ind, double bound) {
+    int pos = -1;
+    int old_prev = -1;
+    for (int i = 0; i < first_in_chunk.size(); ++i) {
+      if (order_number < size_of_chunk[i]) {
+        pos = first_in_chunk[i];
+        while (order_number) {
+          old_prev = pos;
+          pos = next[pos];
+          --order_number;
+        }
+        break;
+      }
+      order_number -= size_of_chunk[i];
+    }
+    int old_ind = inds[pos];
+    if (old_ind == new_ind) {
+      return false;
+    }
+
+    diagonal_y += -ranks[old_ind] + ranks[new_ind];
+
+    int old_chunk = old_ind / chunk_length;
+    int new_chunk = new_ind / chunk_length;
+
+    // remove_pos(old_prev, pos);
+    size_of_chunk[old_chunk]--;
+    sum_in_chunk[old_chunk] -= ranks[old_ind];
+    if (old_prev == -1) {
+      first_in_chunk[old_chunk] = next[pos];
+    } else {
+      next[old_prev] = next[pos];
+    }
+
+
+    // int new_prev = add_ind(new_ind, pos);
+    inds[pos] = new_ind;
+    size_of_chunk[new_chunk]++;
+    sum_in_chunk[new_chunk] += ranks[new_ind];
+    int new_prev = -1;
+    int tmp = first_in_chunk[new_chunk];
+    while (tmp != -1 && inds[tmp] <= new_ind) {
+      new_prev = tmp;
+      tmp = next[tmp];
+    }
+
+    if (new_prev == -1) {
+      next[pos] = first_in_chunk[new_chunk];
+      first_in_chunk[new_chunk] = pos;
+    } else {
+      next[pos] = next[new_prev];
+      next[new_prev] = pos;
+    }
+
+    if ((new_prev == -1 || inds[new_prev] < new_ind) && check_es_not_less(bound)) {
+      return true;
+    }
+
+    // remove_pos(new_prev, pos);
+    size_of_chunk[new_chunk]--;
+    sum_in_chunk[new_chunk] -= ranks[new_ind];
+    if (new_prev == -1) {
+      first_in_chunk[new_chunk] = next[pos];
+    } else {
+      next[new_prev] = next[pos];
+    }
+
+
+    // add_ind(old_ind, pos, old_prev);
+    inds[pos] = old_ind;
+    size_of_chunk[old_chunk]++;
+    sum_in_chunk[old_chunk] += ranks[old_ind];
+
+    if (old_prev == -1) {
+      next[pos] = first_in_chunk[old_chunk];
+      first_in_chunk[old_chunk] = pos;
+    } else {
+      next[pos] = next[old_prev];
+      next[old_prev] = pos;
+    }
+
+    diagonal_y += ranks[old_ind] - ranks[new_ind];
+
+    return false;
+  }
+};
+
+int perturbate(const vector<double> &S, vector<int> &p, vector<double> &chunkSum, vector<int> &chunkSize, double bound, mt19937& rng) {
+    double pert_coeff = 0.1;
+  int n = (int) S.size();
+  int k = (int) p.size();
+  genes gns(S, p);
+  uniform_int_distribution<> uid_n(0, n - 1);
+  uniform_int_distribution<> uid_k(0, k - 1);
+  double NS = 0;
+  for (int pos : p) {
+    NS += S[pos];
+  }
+  int moves = 0;
+  int iters = max(1, (int) (k * pert_coeff));
+  for (int i = 0; i < iters; i++) {
+    int pos = uid_k(rng);
+    int new_ind = uid_n(rng);
+    moves += gns.try_replace(pos, new_ind, bound);
+  }
+
+  p = gns.get_inds();
+  return moves;
 }
