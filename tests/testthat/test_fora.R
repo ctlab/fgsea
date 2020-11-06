fbbe490ce81816bfede149d0a5bb24b7a1918765
vdsukhov <vdsukhov@ya.ru>
2020-11-06 12:54:53 +0300

    Fix fora for a single element pathways list

---
+++ tests/testthat/test_fora.R
@@ -0,0 +1,34 @@
+context("fora analysis")
+
+test_that("fora works if pathways consists of one element", {
+    pthw <- c("1", "2", "10")
+    genes <- paste(1:5)
+    universe <- paste(1:10)
+
+
+    res <- fora(list("test_pathway" = pthw), genes, universe)
+    expect_true(nrow(res) == 1)
+    expect_equal(res$pval, phyper(1, 3, 7, 5, lower.tail = FALSE))
+})
+
+
+test_that("fora works if pathways consist of multiple elements", {
+    pthw1 <- c("1", "2", "10")
+    pthw2 <- paste(1:4)
+    pthw3 <- paste(6:8)
+
+    genes <- paste(1:5)
+    universe <- paste(1:10)
+
+
+    res <- fora(list("test_pathway_1" = pthw1,
+                     "test_pathway_2" = pthw2,
+                     "test_pathway_3" = pthw3),
+                genes, universe)
+
+    expect_true(nrow(res) == 3)
+
+    expect_equal(res$pval, sort(c(phyper(1, 3, 7, 5, lower.tail = FALSE),
+                                  phyper(3, 4, 6, 5, lower.tail = FALSE),
+                                  phyper(-1, 3, 7, 5, lower.tail = FALSE))))
+})
