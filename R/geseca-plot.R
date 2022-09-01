3c98da110050a0ef4600f64e6c64b6a03eb31999
Alexey Sergushichev <alsergbox@gmail.com>
2022-09-01 20:30:25 +0300

    plotCoregulationProfile draft

---
+++ R/geseca-plot.R
@@ -0,0 +1,54 @@
+#'
+#' @export
+#' @import data.table ggplot2
+plotCoregulationProfile <- function(pathway, E,
+                                    titles=colnames(E),
+                                    conditions=NULL) {
+    E <- t(scale(t(E), scale = FALSE))
+
+    genes <- pathway
+
+    dt <- as.data.table(E[rownames(E) %in% genes, ], keep.rownames = TRUE)
+
+    colnames(dt) <- c("gene", titles)
+
+    dt[, id := seq_len(.N)]
+
+    mdt <- melt(dt, measure.vars = colnames(dt)[2:(ncol(dt) - 1)], value.name = "expressionValue",
+                variable.name = "sample",id.vars = c("id", "gene"))
+    mdt[, gene := as.factor(gene)]
+    mdt[, sample := factor(sample, levels=titles)]
+
+    pointDt <- data.table(x = seq_len(ncol(E)),
+                          y = colSums(E[rownames(E) %in% genes, ]) / sum(rownames(E) %in% genes),
+                          condition=if (!is.null(conditions)) { conditions  } else "x")
+
+
+    profilePlot <- ggplot(mdt, aes(x=sample, y=expressionValue, group=gene, color=gene),
+                          show.legend=FALSE) +
+        scale_color_discrete(guide="none") +
+        geom_point(alpha = 0.1) +
+        geom_path(alpha = 0.2) +
+        geom_line(data = pointDt, aes(x = x, y = y),
+                  group = "mean", color = "#13242a", size = 1.5) +
+        geom_hline(yintercept = min(pointDt$y), color = "#495057", linetype = "dashed", size = 1) +
+        geom_hline(yintercept = max(pointDt$y), color = "#495057", linetype = "dashed", size = 1) +
+        (if (!is.null(conditions)) {
+            geom_point(shape=21, size=4,
+                       data = pointDt,
+                       aes(x = x, y = y, fill=condition),
+                       group="mean", color="black")
+        } else {
+            geom_point(shape=21, size=4,
+                       data = pointDt,
+                       aes(x = x, y = y),
+                       fill="black",
+                       group="mean", color="black")
+        }) +
+        theme_classic(base_size = 16) +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+        ylab("expression") +
+        NULL
+
+    profilePlot
+}
