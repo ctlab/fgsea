2045bfc86bd7a85d12b56564978442bd3c0390f2
Alexey Sergushichev <alsergbox@gmail.com>
2022-09-02 11:57:13 +0300

    plotGseaTable: options to control style parameters, #29

--- R/plot.R
+++ R/plot.R
@@ -1,3 +1,25 @@
+cowplotText <- function(text, style) {
+    ggdraw() + do.call(cowplot::draw_text, c(list(text=text), style))
+}
+
+cowplotLabel <- function(label, style) {
+    ggdraw() + do.call(cowplot::draw_label, c(list(label=label), style))
+}
+
+valueToExpExpression <- function(val, log2err=NULL) {
+    a <- floor(log10(val))
+    b <- val/(10**a)
+    x <- sprintf("%.1f\u00B710", b)
+    if (is.null(log2err)) {
+        y <- as.character(a)
+    } else {
+        y <- sprintf("%s±%.2f", a, log2err / log2(10))
+    }
+
+    as.expression(substitute(x ^ y, list(y=y,  x=x)))
+}
+
+
 #' Plots table of enrichment graphs using ggplot and gridExtra.
 #' @param pathways Pathways to plot table, as in `fgsea` function.
 #' @param stats Gene-level stats, as in `fgsea` function.
@@ -8,6 +30,13 @@
 #' @param colwidths Vector of five elements corresponding to column width for
 #'      grid.arrange. Can be both units and simple numeric vector, in latter case
 #'      it defines proportions, not actual sizes. If column width is set to zero, the column is not drawn.
+#' @param pathwayLabelStyle list with style parameter adjustments for pathway labels.
+#'      For example, `list(size=10, color="red")` set the font size to 10 and color to red.
+#'      See `cowplot::draw_text` for possible options.
+#' @param headerLabelStyle similar to `pathwayLabelStyle` but for the table header.
+#' @param valueStyle similar to `pathwayLabelStyle` but for NES and p-value columns.
+#' @param axisLabelStlye list with style parameter adjustments for stats axis labels.
+#'      See `ggplot2::element_text` for possible options.
 #' @param render (deprecated)
 #' @return ggplot object with enrichment barcode plots
 #' @import ggplot2
@@ -24,7 +53,20 @@
 plotGseaTable <- function(pathways, stats, fgseaRes,
                           gseaParam=1,
                           colwidths=c(5, 3, 0.8, 1.2, 1.2),
+                          pathwayLabelStyle=NULL,
+                          headerLabelStyle=NULL,
+                          valueStyle=NULL,
+                          axisLabelStyle=NULL,
                           render=NULL) {
+
+    pathwayLabelStyleDefault <- list(size=12, hjust=1, x=0.95, vjust=0)
+    pathwayLabelStyle <- modifyList(pathwayLabelStyleDefault, as.list(pathwayLabelStyle))
+
+    headerLabelStyleDefault <- list(size=12)
+    headerLabelStyle <- modifyList(headerLabelStyleDefault, as.list(headerLabelStyle))
+
+    valueStyleDefault <- list(size=12, vjust=0)
+    valueStyle <- modifyList(valueStyleDefault, as.list(valueStyle))
 
     if (!is.null(render)) {
         warning("render argument is deprecated, a ggplot object is always returned")
@@ -48,7 +90,7 @@ plotGseaTable <- function(pathways, stats, fgseaRes,
         p <- pathways[[pn]]
         annotation <- fgseaRes[match(pn, fgseaRes$pathway), ]
         list(
-            textGrob(pn, just="right", x=unit(0.95, "npc")),
+            cowplotText(pn, pathwayLabelStyle),
             ggplot() +
                 geom_segment(aes(x=p, xend=p,
                                  y=0, yend=statsAdj[p]),
@@ -67,9 +109,9 @@ plotGseaTable <- function(pathways, stats, fgseaRes,
                       plot.margin = rep(unit(0,"null"),4),
                       panel.spacing = rep(unit(0,"null"),4)
                 ),
-            textGrob(sprintf("%.2f", annotation$NES)),
-            textGrob(sprintf("%.1e", annotation$pval)),
-            textGrob(sprintf("%.1e", annotation$padj))
+            cowplotText(sprintf("%.2f", annotation$NES), valueStyle),
+            cowplotLabel(valueToExpExpression(annotation$pval), valueStyle),
+            cowplotLabel(valueToExpExpression(annotation$padj), valueStyle)
             )
     })
 
@@ -87,13 +129,16 @@ plotGseaTable <- function(pathways, stats, fgseaRes,
               axis.ticks.y=element_blank(),
               panel.grid = element_blank(),
               axis.title=element_blank(),
+              axis.text=do.call(element_text, as.list(axisLabelStyle)),
               plot.margin = unit(c(0,0,0.5,0), "npc"),
               panel.spacing = unit(c(0,0,0,0), "npc")
         )
 
     grobs <- c(
-        list(textGrob("Pathway", just="right", x=unit(0.95, "npc"))),
-        lapply(c("Gene ranks", "NES", "pval", "padj"), textGrob),
+        list(cowplotText("Pathway",
+                         modifyList(headerLabelStyle, pathwayLabelStyle[c("hjust", "x")])
+                         )),
+        lapply(c("Gene ranks", "NES", "pval", "padj"), cowplotText, style=headerLabelStyle),
         unlist(ps, recursive = FALSE),
         list(nullGrob(),
              rankPlot,
